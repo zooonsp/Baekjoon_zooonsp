8f0d92eb9774770d9fb8c1228cb66145c735aa68
zooonsp <109584771+zooonsp@users.noreply.github.com>
2023-04-11 22:04:20 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.04 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
+    vector<int> answer;
+    return answer;
+}
